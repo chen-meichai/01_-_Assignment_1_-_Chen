0bd30b4a37e9652759a456420e9590885d0d4498
chen-meichai <meichaichen1116@gmail.com>
2023-09-24 14:31:19 -0500

    corrected batch file to work on current project

--- batch.bat
+++ batch.bat
@@ -1,9 +1,5 @@
 :: Generate document in html and pdf formats
-quarto.exe render ./src/qmd/iris.qmd  --to html,pdf
+quarto.exe render .\src\qmd\population-dynamics.qmd  --to html
 
 :: Move output to appropriate location
 move /y .\src\qmd\*.html .\doc
-move /y .\src\qmd\*.pdf  .\doc
-
-:: Extract code from qmd file into a self-contained R script
-R.exe CMD BATCH --vanilla .\src\R\purl_code.R
